8fdc23c7ecbcf6aca1bbde3ae727eeee39b7a1cd
Nam <superbigsoft@gmail.com>
2025-10-12 10:51:19 +1100

    Update SpeechRecognition.m

--- src/ios/SpeechRecognition.m
+++ src/ios/SpeechRecognition.m
@@ -23,11 +23,16 @@
 @property (strong, nonatomic) AVAudioEngine *audioEngine;
 @property (strong, nonatomic) SFSpeechAudioBufferRecognitionRequest *recognitionRequest;
 @property (strong, nonatomic) SFSpeechRecognitionTask *recognitionTask;
+@property (strong, nonatomic) AVAudioRecorder *avAudioRecorder;
 
 @end
 
 
 @implementation SpeechRecognition
+
+- (void)pluginInitialize {
+    self.audioEngine = [[AVAudioEngine alloc] init];
+}
 
 - (void)isRecognitionAvailable:(CDVInvokedUrlCommand*)command {
     CDVPluginResult *pluginResult = nil;
@@ -38,6 +43,11 @@
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:NO];
     }
 
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)returnErrorForCommand:(CDVInvokedUrlCommand*)command andError:(NSError *)error {
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.description];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
@@ -70,27 +80,48 @@
         int matches = [[command argumentAtIndex:1 withDefault:@(DEFAULT_MATCHES)] intValue];
         BOOL showPartial = [[command argumentAtIndex:3 withDefault:@(NO)] boolValue];
 
-        NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:language];
-        self.speechRecognizer = [[SFSpeechRecognizer alloc] initWithLocale:locale];
-        self.audioEngine = [[AVAudioEngine alloc] init];
 
         // Cancel the previous task if it's running.
         if ( self.recognitionTask ) {
             [self.recognitionTask cancel];
+            [self.recognitionTask finish];
             self.recognitionTask = nil;
         }
 
+        [self.audioEngine reset];
         AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
-        [audioSession setMode:AVAudioSessionModeDefault error:nil];
-        [audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
-
-        self.recognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
-        self.recognitionRequest.shouldReportPartialResults = showPartial;
-
+        
+        NSError *error = nil;
+        
+        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error: &error];
+        
+        if (error) {
+            [self returnErrorForCommand:command andError:error];
+            return;
+        }
+        
+        [audioSession setMode:AVAudioSessionModeDefault error:&error];
+        if (error) {
+            [self returnErrorForCommand:command andError:error];
+            return;
+        }
+        
+        [audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:&error];
+        
+        if (error) {
+            [self returnErrorForCommand:command andError:error];
+            return;
+        }
+        
         AVAudioInputNode *inputNode = self.audioEngine.inputNode;
         AVAudioFormat *format = [inputNode outputFormatForBus:0];
+        NSLog(@"format channelCount: %d, sampleRate: %f", format.channelCount, format.sampleRate);
+        NSLog(@"inputNode channelCount: %d, sampleRate: %f", [inputNode outputFormatForBus:0].channelCount, [inputNode outputFormatForBus:0].sampleRate);
 
+        NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:language];
+        self.speechRecognizer = [[SFSpeechRecognizer alloc] initWithLocale:locale];
+        self.recognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
+        self.recognitionRequest.shouldReportPartialResults = showPartial;
         self.recognitionTask = [self.speechRecognizer recognitionTaskWithRequest:self.recognitionRequest resultHandler:^(SFSpeechRecognitionResult *result, NSError *error) {
 
             if ( result ) {
@@ -109,7 +140,7 @@
 
                 NSLog(@"startListening() recognitionTask result array: %@", transcriptions.description);
 
-                CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:transcriptions];
+                CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"isFinal": @(result.isFinal), @"isPartial": @(!result.isFinal), @"matches": transcriptions}];
                 if (showPartial){
                     [pluginResult setKeepCallbackAsBool:YES];
                 }
@@ -118,12 +149,7 @@
 
             if ( error ) {
                 NSLog(@"startListening() recognitionTask error: %@", error.description);
-
-                [self.audioEngine stop];
-                [self.audioEngine.inputNode removeTapOnBus:0];
-
-                self.recognitionRequest = nil;
-                self.recognitionTask = nil;
+                [self stopAll];
 
                 CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.description];
                 if (showPartial){
@@ -134,24 +160,118 @@
 
             if ( result.isFinal ) {
                 NSLog(@"startListening() recognitionTask isFinal");
-
-                [self.audioEngine stop];
-                [self.audioEngine.inputNode removeTapOnBus:0];
-
-                self.recognitionRequest = nil;
-                self.recognitionTask = nil;
+                [self stopAll];
             }
         }];
+        
 
-        [inputNode installTapOnBus:0 bufferSize:1024 format:format block:^(AVAudioPCMBuffer *buffer, AVAudioTime *when) {
-            [self.recognitionRequest appendAudioPCMBuffer:buffer];
-        }];
 
-        [self.audioEngine prepare];
-        [self.audioEngine startAndReturnError:nil];
+        [inputNode removeTapOnBus:0];
+        @try {
+            [inputNode installTapOnBus:0 bufferSize:1024 format:format block:^(AVAudioPCMBuffer *buffer, AVAudioTime *when) {
+                [self.recognitionRequest appendAudioPCMBuffer:buffer];
+            }];
 
+            [self.audioEngine prepare];
+            [self.audioEngine startAndReturnError:nil];
+            self.avAudioRecorder = [self record];
+        }
+        @catch (NSException *error) {
+            CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.description];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            [self stopAll];
+        }
     }];
 
+}
+
+- (void)stopAll {
+    [self stopAudioRecording];
+    
+    [self.audioEngine stop];
+    [self.audioEngine.inputNode removeTapOnBus:0];
+
+    [self.recognitionRequest endAudio];
+    self.recognitionRequest = nil;
+    
+    [self.recognitionTask cancel];
+    [self.recognitionTask finish];
+    self.recognitionTask = nil;
+}
+
+- (NSString *) dateString
+{
+    // return a formatted string for a file name
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"ddMMMYY_hhmmssa";
+    return [[formatter stringFromDate:[NSDate date]] stringByAppendingString:@".m4a"];
+}
+
+- (AVAudioRecorder*) record
+{
+    NSError *error;
+
+    // Recording settings
+    NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+
+    [settings setValue: [NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [settings setValue: [NSNumber numberWithFloat:8000.0] forKey:AVSampleRateKey];
+    [settings setValue: [NSNumber numberWithInt: 1] forKey:AVNumberOfChannelsKey];
+    [settings setValue: [NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+    [settings setValue: [NSNumber numberWithBool:NO] forKey:AVLinearPCMIsBigEndianKey];
+    [settings setValue: [NSNumber numberWithBool:NO] forKey:AVLinearPCMIsFloatKey];
+    [settings setValue:  [NSNumber numberWithInt: AVAudioQualityMax] forKey:AVEncoderAudioQualityKey];
+     NSArray *searchPaths =NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentPath_ = [searchPaths objectAtIndex: 0];
+
+    NSString *pathToSave = [documentPath_ stringByAppendingPathComponent:[self dateString]];
+
+    // File URL
+    NSURL *url = [NSURL fileURLWithPath:pathToSave];//FILEPATH];
+
+    // Create recorder
+    AVAudioRecorder* recorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:&error];
+    if (!recorder)
+    {
+        NSLog(@"Error establishing recorder: %@", error.localizedFailureReason);
+        return nil;
+    }
+
+    recorder.delegate = self;
+    if (![recorder prepareToRecord])
+    {
+        NSLog(@"Error: Prepare to record failed");
+        //[self say:@"Error while preparing recording"];
+        return nil;
+    }
+
+    if (![recorder record])
+    {
+        NSLog(@"Error: Record failed");
+    //  [self say:@"Error while attempting to record audio"];
+        return nil;
+    }
+
+    return recorder;
+}
+
+- (void) stopAudioRecording {
+    if (nil != self.avAudioRecorder) {
+        [self.avAudioRecorder stop];
+    }
+}
+
+- (void) playRecording:(NSURL *) url
+{
+    NSLog(@"playRecording");
+    // Init audio with playback capability
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryPlayback error:nil];
+    NSError *error;
+    AVAudioPlayer *audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    audioPlayer.numberOfLoops = 0;
+    [audioPlayer play];
+    NSLog(@"playing");
 }
 
 - (void)stopListening:(CDVInvokedUrlCommand*)command {
@@ -164,12 +284,15 @@
             [audioSession setCategory:AVAudioSessionCategoryPlayback error:nil];
             [audioSession setMode:AVAudioSessionModeDefault error:nil];
             [audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
-        
-            [self.audioEngine stop];
-            [self.recognitionRequest endAudio];
         }
 
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.audioEngine stop];
+        if (nil != self.recognitionRequest) {
+            [self.recognitionRequest endAudio];
+        }
+        [self stopAudioRecording];
+
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:self.avAudioRecorder.url.absoluteString];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
 }
